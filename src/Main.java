b7813ae071401b49e906bd29016b6f6e35e119c6
Matheus Henrique <mathsoares1@gmail.com>
2019-05-13 13:42:57 -0400

    Resolvido problema do newline

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         String input = "";
-
         FileReader fileReader = new FileReader("code.xpp");
 
         try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             String line;
             while((line = bufferedReader.readLine()) != null) {
-                input += (line);
+                input += (line + "\n");
             }
         }
 
